2de60fe012e475f47d71ac836b92fb82b4500c0e
olusegun Owolabi <olujawo1996@gmail.com>
2022-06-20 15:08:33 +0100

    Creating the layout of the Application

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Chat from './Components/Chat'
+import Login from './Components/Login'
+import styled from 'styled-components'
+import Header from './Components/Header'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Container>
+          <Header/>   
+          <Main>
+            <Sidebar/>
+             <Switch>
+              <Route path = "/room">
+                <Chat />
+              </Route>
+              <Route path="/">
+                <Login />
+              </Route>
+             </Switch>
+          </Main>
+        </Container>
+      </Router>
     </div>
   );
 }
 
 export default App;
+
+const Container = styled.div`
+    width: 100%;
+    height: 100vh;
+    background: orange;
+    display: grid;
+    grid-template-rows: 38px auto;
+`
+const Main = styled.div`
+background: blue;
+height
+`
