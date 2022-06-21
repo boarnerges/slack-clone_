23cea86ed04e5d4185f993196360e6019a1a165b
olusegun Owolabi <olujawo1996@gmail.com>
2022-06-21 05:21:17 +0100

    completed header and sidebar

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,11 +1,130 @@
+import { Container } from '@material-ui/core'
 import React from 'react'
+import styled from 'styled-components'
+import AddCircleOutlineIcon from '@mui/icons-material/AddCircleOutline';
+import {SidebarItemsData} from '../data/SidebarData'
+import AddIcon from '@mui/icons-material/Add';
 
 function Sidebar() {
   return (
-    <div>Sidebar
+    <Container>
+      <WorkSpaceContainer>
+        <Name>
+          Jim Moriaty
+        </Name>
+        <NewMessage>
+          <AddCircleOutlineIcon/>
+        </NewMessage>
+      </WorkSpaceContainer>
+      <MainChannels>
+          {
+              SidebarItemsData.map( item => (
+                  <MainChannelItem>
+                    {item.icon}
+                    {item.text}
+                  </MainChannelItem>
+                )
+              )
+            }
+        )
+        <MainChannelItem>
+          <AddCircleOutlineIcon />
+          Add
+        </MainChannelItem>
+      </MainChannels>
+      <ChannelsContainer>
+        <NewChannelContainer>
+          <ChannelList>
+            <Channel>
+              #Channel
+            </Channel>
+            <Channel>
+              #Channel
+            </Channel>
 
-    </div>
+          </ChannelList>
+        <AddIcon />  
+        </NewChannelContainer>
+       
+      </ChannelsContainer>
+    </Container>
   )
 }
 
 export default Sidebar
+
+const Container = styled.div`
+    background: #3F0E40;
+`
+
+const WorkSpaceContainer = styled.div`
+    color: white;
+    height: 64px;
+    display: flex;
+    align-items: center;
+    padding-left: 19px
+    justify-content: space-between;
+    border-bottom: 1px solid #532753;
+`
+
+const Name = styled.div`
+
+`
+
+const NewMessage = styled.div`
+     width: 36px;
+     height: 36px;
+     background: white;
+     color: #3F0E40;
+     fill: #3F0E40;
+     display: flex;
+     justify-content: center;
+     align-items: center;
+     border-radius: 50%;
+     margin-right: 20px;
+     cursor: pointer;
+`
+const MainChannels = styled.div`
+      padding-top: 20px;
+`
+
+const MainChannelItem = styled.div`
+      color: rgb(188, 171, 188);
+      dispaly: grid;
+      grid-template-columns: 15% auto;
+      height: 28px;
+      align-items: center;
+      padding-left: 19px;
+      cursor: pointer;
+      :hover {
+        background: #350D36;
+      }
+`
+
+const ChannelsContainer =styled.div`
+      color: rgb(188, 171, 188);
+      Margin-top: 10px
+`
+
+const NewChannelContainer = styled.div`
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      height: 28px;
+      padding-left: 19px;
+      padding-right: 12px;
+`
+
+const ChannelList = styled.div`
+
+`
+const Channel = styled.div`
+    height: 28px
+    dispaly: flex;
+    aligh-items: center;
+    padding-left: 19px
+    cursor: pointer;
+    :hover {
+      background: #350D36;
+    }
+`
---
+++ src/data/SidebarData.js
@@ -0,0 +1,45 @@
+import MessageIcon from '@mui/icons-material/Message';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
+import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
+import AppsIcon from '@mui/icons-material/Apps';
+
+export const SidebarItemsData = [
+
+{
+    icon: <MessageIcon />,
+    text: "Thread"
+
+}, 
+
+{
+    icon: <InboxIcon />,
+    text: "All DMs"
+
+},
+
+{
+    icon: <DraftsIcon />,
+    text: "Mentions & Reactions"
+
+},
+
+{
+    icon: <BookmarkBorderIcon />,
+    text: "Save Items"
+
+},
+{
+    icon: <PeopleAltIcon />,
+    text: "Peoples & Groups"
+
+},
+
+{
+    icon: <AppsIcon />,
+    text: "More"
+
+}
+
+]
