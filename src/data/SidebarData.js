794047a64e700f1af7cb99a18a0dfd223eee7917
olusegun Owolabi <olujawo1996@gmail.com>
2022-06-30 18:37:57 +0100

    loginjs

--- src/data/SidebarData.js
+++ src/data/SidebarData.js
@@ -1,12 +1,5 @@
  // eslint-disable-next-line
 import React, { Component }  from 'react';
-// import MessageIcon from '@mui/icons-material/Message';
-// import InboxIcon from '@mui/icons-material/Inbox';
-// import DraftsIcon from '@mui/icons-material/Drafts';
-// import BookmarkBorderIcon from '@mui/icons-material/BookmarkBorder';
-// import PeopleAltIcon from '@mui/icons-material/PeopleAlt';
-// import AppsIcon from '@mui/icons-material/Apps';
-
 
 export const SidebarItemsData = [
 
